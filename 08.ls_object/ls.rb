e181af7049e0fed5010e48409a4ee8d439213712
asamih <a.hirose78@gmail.com>
2021-03-13 11:32:25 +0900

    変数名の省略の修正

--- 08.ls_object/ls.rb
+++ 08.ls_object/ls.rb
@@ -20,38 +20,38 @@ module LS
     end
 
     def data(files)
-      fdata = []
-      files.each do |fname|
-        fstat = ::File::Stat.new(fname)
-        fdata.push [
-          permission_convert(fstat),
-          fstat.nlink.to_s.rjust(nlink_length(files)),
-          Etc.getpwuid(fstat.uid).name,
-          Etc.getgrgid(fstat.gid).name.rjust(6),
-          fstat.size.to_s.rjust(5),
-          fstat.mtime.strftime('%_m %e %H:%M'),
-          fname
+      file_data = []
+      files.each do |file_name|
+        file_stat = ::File::Stat.new(file_name)
+        file_data.push [
+          permission_convert(file_stat),
+          file_stat.nlink.to_s.rjust(nlink_length(files)),
+          Etc.getpwuid(file_stat.uid).name,
+          Etc.getgrgid(file_stat.gid).name.rjust(6),
+          file_stat.size.to_s.rjust(5),
+          file_stat.mtime.strftime('%_m %e %H:%M'),
+          file_name
         ]
       end
-      fdata
+      file_data
     end
 
     private
 
     def nlink_length(files)
       max_length = []
-      files.each do |fname|
-        fstat = ::File::Stat.new(fname)
-        max_length << fstat.nlink
+      files.each do |file_name|
+        file_stat = ::File::Stat.new(file_name)
+        max_length << file_stat.nlink
       end
       max_length.max_by { |num| num.to_s.length }.to_s.length
     end
 
-    def permission_convert(fstat)
-      type = fstat.ftype == 'file' ? '-' : fstat.ftype[0]
+    def permission_convert(file_stat)
+      type = file_stat.ftype == 'file' ? '-' : file_stat.ftype[0]
       rwx = { '0' => '---', '1' => '--x', '2' => '-w-', '3' => '-wx',
               '4' => 'r--', '5' => 'r-x', '6' => 'rw-', '7' => 'rwx' }
-      mode = fstat.mode.to_s(8)[-3, 3].gsub(/\d/, rwx)
+      mode = file_stat.mode.to_s(8)[-3, 3].gsub(/\d/, rwx)
       "#{type}#{mode} "
     end
   end
@@ -89,9 +89,9 @@ module LS
   class HorizontalFormatter
     def multi_column(files)
       result = []
-      files.map { |fname| result << fname.to_s.ljust(24, ' ') }
+      files.map { |file_name| result << file_name.to_s.ljust(24, ' ') }
       format = result.each_slice(length(files)).to_a
-      push_nil(format).transpose.each { |a| puts a.join('') }
+      push_nil(format).transpose.each { |array| puts array.join('') }
     end
 
     private
@@ -102,24 +102,24 @@ module LS
 
     def push_nil(format)
       max = format.max_by(&:length).length
-      format.each { |a| a[max - 1] = nil if a.length < max }
+      format.each { |array| array[max - 1] = nil if array.length < max }
     end
   end
 
   class VerticalFormatter
     def total(files)
       total = 0
-      files.each do |fname|
-        fstat = ::File::Stat.new(fname)
-        total += fstat.blocks
+      files.each do |file_name|
+        file_stat = ::File::Stat.new(file_name)
+        total += file_stat.blocks
       end
       puts "total #{total}"
     end
 
     def single_column(files)
       total(files)
-      fdata = LS::Command.new.data(files)
-      fdata.map { |a| puts a.each_slice(7).to_a.join(' ') }
+      file_data = LS::Command.new.data(files)
+      file_data.map { |array| puts array.each_slice(7).to_a.join(' ') }
     end
   end
 end
