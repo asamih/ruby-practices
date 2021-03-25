6e7d439d1ab121a1ba031b0c7ec4c127c7224f9c
asamih <a.hirose78@gmail.com>
2021-03-25 12:13:37 +0900

    スペルミスの修正

--- 09.wc_object/wc.rb
+++ 09.wc_object/wc.rb
@@ -32,10 +32,10 @@ module Wc
           number = 0
           results = file_elements.map do |file_data|
             number += 1
-            number % 4 != 0 ? rayout(file_data) : " #{file_data}\n"
+            number % 4 != 0 ? layout(file_data) : " #{file_data}\n"
           end
         else
-          results = file_elements.map { |file_data| rayout(file_data) }
+          results = file_elements.map { |file_data| layout(file_data) }
         end
         puts results.join('')
         puts "#{total(file_details(files))} total" if file_elements.length > 4
@@ -48,11 +48,11 @@ module Wc
           number = 0
           file_elements.each do |file_data|
             number += 1
-            results << rayout(file_data) if number == 1 || ((number - 1) % 4).zero?
+            results << layout(file_data) if number == 1 || ((number - 1) % 4).zero?
             results << " #{file_data}\n" if (number % 4).zero?
           end
         else
-          results << rayout(file_elements.first)
+          results << layout(file_elements.first)
         end
         puts results.join('')
         puts "#{total_line(file_details(files))} total" if file_elements.length > 4
@@ -80,17 +80,17 @@ module Wc
                 @byte = file_unit.bytesize
       end
 
-      def rayout(file_data)
+      def layout(file_data)
         file_data.to_s.rjust(8, ' ')
       end
 
       def total(file_details)
         total = file_details.map { |array| array[0..2] }.transpose.map { |file_subtotal| file_subtotal.inject(:+) }
-        total.map { |file_total| rayout(file_total) }.join('')
+        total.map { |file_total| layout(file_total) }.join('')
       end
 
       def total_line(file_details)
-        rayout(file_details.map { |file_subtotal| file_subtotal[0] }.sum)
+        layout(file_details.map { |file_subtotal| file_subtotal[0] }.sum)
       end
     end
   end
